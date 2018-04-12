b999e93ea15ec6b1c1736728692fcb5a46e078a9
Zachary Stuck <30481102+stuckboy@users.noreply.github.com>
2018-04-12 17:45:48 -0400

    Can successfully log in via PHP and MySQL.

--- app/src/main/java/zachstuck/scannertool/LoginActivity.java
+++ app/src/main/java/zachstuck/scannertool/LoginActivity.java
@@ -1,10 +1,21 @@
 package zachstuck.scannertool;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * Created by Zachary Stuck on 3/20/2018
@@ -14,22 +25,25 @@ import android.widget.Button;
 public class LoginActivity extends AppCompatActivity {
 
     Button submitButton, returnButton;
+    EditText userCred, passCred;
+    private String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userCred = findViewById(R.id.userField);
+        passCred = findViewById(R.id.passField);
         submitButton = findViewById(R.id.Login);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Do login checks
-                boolean permitEntry = true;
-                if (permitEntry) {
-                    Intent logIntent = new Intent(LoginActivity.this, UserPageActivity.class);
-                    LoginActivity.this.startActivity(logIntent);
-                    finish();
-                }
+                username = userCred.getText().toString();
+                password = passCred.getText().toString();
+                new AsyncLogin().execute(username, password);
+
             }
         });
         returnButton = findViewById(R.id.goBack);
@@ -46,5 +60,53 @@ public class LoginActivity extends AppCompatActivity {
         Intent finishIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(finishIntent);
         finish();
+    }
+
+    private class AsyncLogin extends AsyncTask<String, String, String> {
+
+        protected String doInBackground(String... args) {
+            try {
+                String username = (String) args[0];
+                String password = (String) args[1];
+
+                String link = "http://euclid.nmu.edu/~zstuck/seniorProjStuff/scannerLogin.php";
+                String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8");
+                data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+
+                URL theURL = new URL(link);
+                URLConnection connection = theURL.openConnection();
+                connection.setDoOutput(true);
+                OutputStreamWriter outWriter = new OutputStreamWriter(connection.getOutputStream());
+
+                outWriter.write(data);
+                outWriter.flush();
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+
+                return stringBuilder.toString();
+            }
+            catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            Log.d("onPostExecute", result);
+            if (result.equals("Success!")) {
+                Intent logIntent = new Intent(LoginActivity.this, UserPageActivity.class);
+                LoginActivity.this.startActivity(logIntent);
+                finish();
+            }
+            else if (result.equals("Error")) {
+                Toast.makeText(LoginActivity.this, "Error while logging in, please try again.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
